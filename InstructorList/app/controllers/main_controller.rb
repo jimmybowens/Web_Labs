0e4cce61dfcbdad3a204072dc0bf8b442c38064f
Jimmy Bowens <jimmy@jimmybowens.com>
2014-09-21 08:28:04 -0400

    Second Commit

--- InstructorList/app/controllers/main_controller.rb
+++ InstructorList/app/controllers/main_controller.rb
@@ -11,9 +11,18 @@ class MainController < ApplicationController
   
   def list
 	@instructors = Instructor.all
+    
+    
   end
   
   def new_record
+      
+      @instructor_name = params[:instructor_name]
+      @instructor_office_number = params[:instructor_office_number]
+      @instructor_email = params[:instructor_email]
+      
+      @new_instructor = Instructor.create(name: @instructor_email, office_number: @instructor_office_number, email: @instructor_email)
+        
   end
   
   def update_record
